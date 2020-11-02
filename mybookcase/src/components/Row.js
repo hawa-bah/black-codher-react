1a058311bff78d467cf89f28272e80d9e1e3fbbe
hawa-bah <56238080+hawa-bah@users.noreply.github.com>
2020-11-02 19:37:54 +0000

    Now each category has a row, the description pops up when a book is hovered. NEXT: the nav bar needs to be scrolled to the corresponding category

--- mybookcase/src/components/Row.js
+++ mybookcase/src/components/Row.js
@@ -1,25 +1,61 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
+import './Row.css'
 
 const Row = (props) => {
 
-    // let {id, 
-    //     volumeInfo: { title, authors, description, imageLinks: {smallThumbnail, thumbnail}},
-    //     saleInfo: {listPrice}
-    // } = props.rowBooks;
+    const [rowBooks, setRowBooks] = useState([]);
+    const [click, setClick] = useState(false);
+    const [description, setDesciption] = useState('');
+    
 
-
+    useEffect(() => {
+        async function getRowBooks() {
+                let results = await fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:${props.title}`).then(res => res.json());
+                // results is an object{}
+                // results.items is an arry [] of objects {}
+                //rowbooks is an array []
+                if(!results.error){ 
+                    setRowBooks(results.items);
+                }
+                // console.log(rowBooks.items);
+                // let { id,
+                //     volumeInfo: {title, authors, description, imageLinks: {smallThumbnail, thumbnail}},
+                //     saleInfo: {listPrice}
+                // } = rowBooks;
+            }
+        getRowBooks();
+    }, []);
+    console.log(rowBooks);
+    console.log(rowBooks.length);
+    console.log(rowBooks.table);
 
     return (
         <>
-        <div>
-            {/* {title} */}
-        </div>
-        <h2>Testing</h2>
-            
+        <div className="row-div">
+            <h3>{props.title}({rowBooks.length})</h3>
+            <div className="row-books">
+                {rowBooks.map((book)=> (
+                    <>
+                        {/* <div className="row-book" onMouseEnter={()=> {setClick(true); setDesciption(book.volumeInfo.description)}} onMouseLeave={setClick(false)}> */}
+                        {/* <div className="row-book" onClick={()=> {setClick(!click); setDesciption(book.volumeInfo.description)}} > */}
+                        <div className="row-book" onMouseOver={()=> {setClick(true); setDesciption(book.volumeInfo.description)}} onMouseOut={() => setClick(false)} >
+
+
+                            <p>{book.volumeInfo.title}</p>
+                            <img src={book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail } alt={book.volumeInfo.title}></img>
+                        </div>
+                    </>
+                ))}
+                
+            </div>
+            { click &&
+            <div className="description">
+                <p>Description</p>
+                <p>{description}</p> 
+            </div>
+}
+        </div>   
         </>
-
-
-
     )
 };
 
--- mybookcase/src/pages/Categories.js
+++ mybookcase/src/pages/Categories.js
@@ -1,36 +1,89 @@
 import React,{useEffect, useState} from 'react';
-import Row from '../components/Row.js'
+import NavBar from '../components/NavBar.js';
+import Row from '../components/Row.js';
+
 
 const Categories = () => {
 
-    const [Drama, setDrama] = useState([]);
-    const [Horror, setHorror] = useState([]);
+//     const [Drama, setDrama] = useState([]);
+//     const [Horror, setHorror] = useState([]);
 
-    useEffect(() => {
-        async function getRowBooks() {
-                let resultsDrama = await fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:Drama`).then(res => res.json());
-                // let resultsDrama = await fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:Drama&filter=paid-ebooks&print-type=books&projection=lite`).then(res => res.json());
-                console.log(resultsDrama);
-                console.log(resultsDrama.items);
-                if(!resultsDrama.error){
-                    setDrama(resultsDrama.items);
-                }
-                return resultsDrama;
-            }
-        getRowBooks();
-    }, []);
-    
 
-//    getRowBooks();
+//     useEffect(() => {
+//         async function getRowBooks() {
+//                 let resultsDrama = await fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:Drama`).then(res => res.json());
+//                 console.log(resultsDrama);
+//                 console.log(resultsDrama.items);
+//                 if(!resultsDrama.error){
+//                     setDrama(resultsDrama.items);
+//                 }
+
+//                 let resultsHorror = await fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:Horror`).then(res => res.json());
+//                 if(!resultsHorror.error){
+//                     setHorror(resultsHorror.items);
+//                 }
+
+//                 // return resultsDrama;
+
+//             }
+//         getRowBooks();
+//     }, []);
+
+// //    getRowBooks();
 //     console.log(Drama.length);
-
+//         console.log(Drama.table);
 
     return (
         <>
-            <Row  title='Drama' rowBooks={Drama}/>
-            <p>{Drama.length}</p>
-            {/* <div className="rowBooks Drama">
+            <NavBar />
+            <Row title="Adventure" />
+            <Row title="Drama" />
+            <Row title="Horror" />
+            <Row title="self-help" />
+            <Row title="Love" />
+
+
+            {/*<Row title="A" />
+            <Row title="Drama" />
+            <Row title="Drama" /> */}
+
+            {/* <Row  title='Drama' rowBooks={Drama}/> */}
+            {/* <p>{Drama.length}</p>
+            <h2>Drama({Horror.length})</h2>
+            <div className="categories">
+
+            
+            <div className="row">
+                {Drama.map((book)=> {
+                    return(
+                        <>
+                        <div className="row-item row-Drama">
+                            <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title}></img>
+                            <p>{book.volumeInfo.title}</p>
+                        </div>
+                        </>
+                    
+                    )
+                })}
+            </div>
+            
+            <div className="row">
+                <h2>Horror({Horror.length})</h2>
+                {Horror.map((book)=> {
+                    return(
+                        <>
+                        <div className="row-item row-Drama">
+                            <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title}></img>
+                            <p>{book.volumeInfo.title}</p>
+                        </div>
+                        </>
+                    
+                    )
+                })}
+            </div>
             </div> */}
+            
+            
         </>
 
 
